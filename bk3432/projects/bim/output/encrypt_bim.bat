4f84b8e6a64dab6c37875a998be732ea8a1bf71a
spreme <2044451563@qq.com>
2022-01-21 11:20:48 +0800

    first version

---
+++ bk3432/projects/bim/output/encrypt_bim.bat
@@ -0,0 +1,12 @@
+.\output\BinConvert_bim  -oad .\output\bim\bk3432_ble_bim.bin -m 0x2000 -l 0x20000 -e 00000000 00000000 00000000 00000000
+
+copy .\output\bim\bk3432_ble_bim.bin ..\ble_app_ancs\output\bim
+copy .\output\bim\bk3432_ble_bim.bin ..\ble_app_gatt\output\bim
+copy .\output\bim\bk3432_ble_bim.bin ..\ble_app_gatt_128\output\bim
+copy .\output\bim\bk3432_ble_bim.bin ..\ble_app_gatt_muti_chars\output\bim
+copy .\output\bim\bk3432_ble_bim.bin ..\ble_app_mouse\output\bim
+copy .\output\bim\bk3432_ble_bim.bin ..\ble_app_remote\output\bim
+copy .\output\bim\bk3432_ble_bim.bin ..\ble_app_wechat\output\bim
+copy .\output\bim\bk3432_ble_bim.bin ..\rf_test\output\bim
+
+
